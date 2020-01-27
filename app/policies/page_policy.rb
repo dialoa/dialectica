fcd8b6cd88c7d3f7f5389ebda966524ab783a00e
Metaphysiker <s.raess@me.com>
2020-01-27 23:15:30 +0100

    minor changes

--- app/policies/page_policy.rb
+++ app/policies/page_policy.rb
@@ -1,6 +1,20 @@
 class PagePolicy < ApplicationPolicy
 
+  def new?
+    is_user_admin?(@user)
+  end
+  
   def update?
     is_user_admin?(@user)
   end
+
+  def edit?
+    is_user_admin?(@user)
+  end
+
+  def destroy?
+    is_user_admin?(@user)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#welcome'
 
-
   resources :pages
+
+  get '/about-us', to: 'pages#show', id: "about-us"
 
 end
