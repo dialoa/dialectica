fae3458b4f9640fb475deac4afd35cdd4aa70c5e
Metaphysiker <s.raess@me.com>
2020-01-27 20:00:49 +0100

    working on page

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,4 +3,20 @@ class Page < ApplicationRecord
   friendly_id :title, use: :slugged
   has_rich_text :content
   has_one_attached :cover
+
+
+  before_save :update_search_field
+
+  def update_search_field
+
+    self.search_field =
+    [
+      title,
+      description,
+      keywords,
+      content.to_plain_text,
+      slug
+    ].compact.join(' ')
+
+  end
 end
