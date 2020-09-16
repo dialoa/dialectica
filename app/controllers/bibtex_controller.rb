07b27df0c38b1808661951dac3c41ece6a6cf1d1
Metaphysiker <s.raess@me.com>
2020-09-16 21:04:26 +0200

    added line break

--- app/controllers/bibtex_controller.rb
+++ app/controllers/bibtex_controller.rb
@@ -9,11 +9,18 @@ class BibtexController < ApplicationController
     #b = BibTeX.open('./public/bibtex/test.bib')
     #b['x'].title = "New Entry"
     #b.save
+    @results = []
 
-    serrano = Serrano.works(query: text)
+    text.split("\n").each do |line|
+      serrano = Serrano.works(query: text)
+      next if serrano.nil?
+      @results.push(Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "text", style: "elsevier-harvard").force_encoding(Encoding::UTF_8))
+    end
+
+
 
     #@result = serrano["message"]["items"].first["DOI"]
-    @result = Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "text", style: "elsevier-harvard").force_encoding(Encoding::UTF_8)
+
 
     #send_data bib, filename: "biblixy.bib"
   end
