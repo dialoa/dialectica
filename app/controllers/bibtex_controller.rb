7b7824530af7ce8a51354256b37e653c8abd3fbf
Metaphysiker <s.raess@me.com>
2020-09-18 22:44:21 +0200

    trying to job out for long list

--- app/controllers/bibtex_controller.rb
+++ app/controllers/bibtex_controller.rb
@@ -9,18 +9,20 @@ class BibtexController < ApplicationController
   def bibtex_create
     text = params[:text]
     format = params[:format]
+
+    CreateBibtexFileJob.perform_later(text, format)
     #bib = BibTeX::Bibliography.new
     #b = BibTeX.open('./public/bibtex/test.bib')
     #b['x'].title = "New Entry"
     #b.save
     #@results = []
 
-    if format == "bib"
-      file = ""
+    #if format == "bib"
+    #  file = ""
 
-      text.split("\n").each do |line|
-        serrano = Serrano.works(query: line)
-        file = file + "\n\n" + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "bibtex").force_encoding(Encoding::UTF_8)
+    #  text.split("\n").each do |line|
+    #    serrano = Serrano.works(query: line)
+    #    file = file + "\n\n" + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "bibtex").force_encoding(Encoding::UTF_8)
         #@results.push(Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "text", style: "elsevier-harvard").force_encoding(Encoding::UTF_8))
         #next if serrano.nil?
         #byebug
@@ -31,29 +33,29 @@ class BibtexController < ApplicationController
         #file = file + "\n\n" + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "csl").force_encoding(Encoding::UTF_8)
         #file = file + "\n\n" + serrano["message"]["items"].first.to_s
         #####file = file + "\n\n" + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "bibtex").force_encoding(Encoding::UTF_8)
-      end
-      send_data file, filename: "references.bib"
-    elsif format == "json"
-      json = []
-      text.split("\n").each do |line|
-        serrano = Serrano.works(query: line)
-        json.push(serrano["message"]["items"].first)
-      end
+    #  end
+    #  send_data file, filename: "references.bib"
+    #elsif format == "json"
+    #  json = []
+    #  text.split("\n").each do |line|
+    #    serrano = Serrano.works(query: line)
+    #    json.push(serrano["message"]["items"].first)
+    #  end
 
       #file = file + "\n\n" + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "bibtex").force_encoding(Encoding::UTF_8)
-      send_data json.to_json, filename: "references.json"
+    #  send_data json.to_json, filename: "references.json"
 
-    elsif format == "citeproc-json"
-      json = {"email" => "john@doe.com", "name" => "John Doe"}
-      json = json.to_json
-      text.split("\n").each do |line|
-        serrano = Serrano.works(query: line)
+    #elsif format == "citeproc-json"
+    #  json = {"email" => "john@doe.com", "name" => "John Doe"}
+    #  json = json.to_json
+    #  text.split("\n").each do |line|
+    #    serrano = Serrano.works(query: line)
         #json.push(serrano["message"]["items"].first)
-        json = Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "json", style: "apa")
-      end
+    #    json = Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "json", style: "apa")
+    #  end
 
       #file = file + "\n\n" + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "bibtex").force_encoding(Encoding::UTF_8)
-      send_data json, filename: "citeproc.json"
+    #  send_data json, filename: "citeproc.json"
     end
 
 
---
+++ app/jobs/create_bibtex_file_job.rb
@@ -0,0 +1,36 @@
+class CreateBibtexFileJob < ApplicationJob
+  queue_as :default
+
+  def perform(text, format)
+    if format == "bib"
+      file = ""
+      text.split("\n").each do |line|
+        serrano = Serrano.works(query: line)
+        file = file + "\n\n" + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "bibtex").force_encoding(Encoding::UTF_8)
+        sleep 1
+      end
+
+      #send_data file, filename: "references.bib"
+      #file = create_data file, filename: "references.bib"
+      file_to_store = "references.bib"
+      Tempfile.open(file_to_store, "w"){|f| f << file}
+
+      stuff = Stuff.create(filename: "references-#{Date.today.to_s}")
+      stuff.file.attach(file_to_store)
+
+    elsif format == "json"
+      json = []
+      text.split("\n").each do |line|
+        serrano = Serrano.works(query: line)
+        json.push(serrano["message"]["items"].first)
+        sleep 1
+      end
+      #send_data json.to_json, filename: "references.json"
+      file_to_store = "references.json"
+      Tempfile.open(file_to_store, "w"){|f| f << json.to_json}
+
+      stuff = Stuff.create(filename: "references-#{Date.today.to_s}")
+      stuff.file.attach(file_to_store)
+    end
+  end
+end
