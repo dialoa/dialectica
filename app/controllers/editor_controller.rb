226dfe8350ac43895823e0c0500a91102919d9c3
Metaphysiker <s.raess@me.com>
2020-09-24 20:41:06 +0200

    minor changes

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -10,11 +10,22 @@ class EditorController < ApplicationController
 
 and other words *italic* with Markdown.
 
-You can even [link to Google!](http://google.com)"
+You can even [link to Google!](http://google.com)
+
+# h1 Heading
+
++ Create a list by starting a line with `+`, `-`, or `*`
+  + Sub-lists are made by indenting 2 spaces:
+    - Marker character change forces new list start:
+      * Ac tristique libero volutpat at
+      + Facilisis in pretium nisl aliquet
+      - Nulla volutpat aliquam velit
+
+  > This is a blockquote"
 
     markdown_text = @search_inputs[:text]
 
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
 
     @result = markdown.render(markdown_text)
   end
