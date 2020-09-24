f8f73da7b2ba6458595486c85f9c809bd16ee787
Metaphysiker <s.raess@me.com>
2020-09-25 00:37:03 +0200

    kramdown works

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,32 +1,26 @@
+require 'kramdown'
+
 class EditorController < ApplicationController
   def editor
+
     if params[:search_inputs].present?
       @search_inputs = OpenStruct.new(params[:search_inputs])
     else
       @search_inputs = OpenStruct.new()
     end
 
-    @search_inputs[:text] = @search_inputs[:text] ? @search_inputs[:text] : "It's very easy to make some words **bold**
-
-and other words *italic* with Markdown.
-
-You can even [link to Google!](http://google.com)
-
-# h1 Heading
-
-+ Create a list by starting a line with `+`, `-`, or `*`
-  + Sub-lists are made by indenting 2 spaces:
-    - Marker character change forces new list start:
-      * Ac tristique libero volutpat at
-      + Facilisis in pretium nisl aliquet
-      - Nulla volutpat aliquam velit
-
-  > This is a blockquote"
+    @search_inputs[:text] = @search_inputs[:text] ? @search_inputs[:text] : "# Header \r\n
+ This is <span style=\"color: red\">written in red</span>."
 
     markdown_text = @search_inputs[:text]
 
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown_text = Kramdown::Document.new(' <h1 id="header">Header</h1> <p>This is <span style="color: red">written in red</span>.</p>').to_kramdown
 
-    @result = markdown.render(markdown_text)
+    #markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+
+    #@result = markdown.render(markdown_text)
+
+    @result = Kramdown::Document.new(markdown_text).to_html
+
   end
 end
