f916348d8db0475899d2078e44a8d7e05fb3d308
Metaphysiker <s.raess@me.com>
2020-10-15 22:49:59 +0200

    using open3

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -43,6 +43,172 @@ code block
 
     @inputs[:text] = @inputs[:text] ? @inputs[:text] : start_markdown
 
+    dir = Rails.root.join('public', 'basic_markdown_editor')
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    File.open(dir.join("basic_markdown_editor.md"), 'w+') do |file|
+      file.write(@inputs[:text])
+    end
+
+    Open3.popen3("pandoc -s -f markdown -t pdf #{dir}/basic_markdown_editor.md") do |stdin, stdout, stderr, wait_thr|
+      stdin.puts "This is sent to the command"
+      stdin.close                # we're done
+      stdout_str = stdout.read   # read stdout to string. note that this will block until the command is done!
+      #puts stdout_str
+
+      file_to_store = Tempfile.new("basic-markdown-editor-#{Date.today.to_s}.pdf")
+      file_to_store.write(stdout_str)
+      file_to_store.rewind
+      @stuff = Stuff.create(filename: "basic_markdown_editor #{DateTime.now}")
+      @stuff.file.attach(io: file_to_store, filename: "basic-markdown-editor-#{Date.today.to_s}.pdf")
+      file_to_store.close
+
+      #stderr_str = stderr.read   # read stderr to string
+      status = wait_thr.value    # will block until the command finishes; returns status that responds to .success? etc
+    end
+
+    File.delete("#{dir}/basic_markdown_editor.md")
+
+  end
+
+  def pancritic_editor
+    if params[:inputs].present?
+      @inputs = OpenStruct.new(params[:inputs])
+    else
+      @inputs = OpenStruct.new()
+    end
+
+    start_markdown ='
+---
+title: Test CriticMarkup with pandoc
+author: Kolen Cheung
+fontfamily: lmodern,changes
+header-includes:
+  \usepackage{lineno}
+  \linenumbers
+  \usepackage{draftwatermark}
+  \SetWatermarkText{dialectica proof}
+  \SetWatermarkScale{.7}
+---
+
+Cum sociis natoquel {--penatibus et magnis--}{>>FTP - 2013-05-13 08:20:18<<} dis parturient montes, nascetur ridiculus mus. Praesent et tellus in eros {++cunning ++}{>>FTP - 2013-05-13 08:20:22<<}eleifend imperdiet non at magna. Nunc aliquam accumsan auctor. In vitae mi sapien. Ut eget pretium purus. {~~Proin~>Prawn~~}{>>FTP - 2013-05-13 08:20:29<<} condimentum hendrerit risus quis tristique. Cras fermentum, diam id sodales feugiat, arcu risus imperdiet nisi, sit amet consequat diam lectus id mi. Cras varius convallis turpis, in iaculis mi cursus vitae. Nulla dignissim aliquet nulla, eu pulvinar nunc fringilla ut. Nullam {==condimentum tortor==}{>>Huh?<<} eu quam tempor tempus. Quisque sit amet magna nec nisl mollis varius a nec ligula. Sed adipiscing, est in gravida sagittis, elit sapien vestibulum quam, a {>>Test comment.<<}tristique arcu eros nec enim. Morbi euismod velit eget ligula faucibus quis feugiat massa fermentum. In velit tellus, pretium ac posuere ac, ultrices eget magna.
+
+1. {++This is an *addition*.++}
+2. {--This was a *deletion*.--}
+3. This {~~*was*~>*is*~~} a substitution.
+4. This is a {==*highlight*==}{>> With a *comment* that should not appear.<<}.
+5. This is a {==*highlight*==} without comment.
+'
+
+
+    @inputs[:text] = @inputs[:text] ? @inputs[:text] : start_markdown
+
+    dir = Rails.root.join('public', 'pancritic')
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    File.open(dir.join("pancritic.md"), 'w+') do |file|
+      file.write(@inputs[:text])
+    end
+
+    Open3.popen3("pancritic -s -o #{dir}/pancritic.pdf #{dir}/pancritic.md") do |stdin, stdout, stderr, wait_thr|
+      stdin.puts "This is sent to the command"
+      stdin.close                # we're done
+      stdout_str = stdout.read   # read stdout to string. note that this will block until the command is done!
+      #puts stdout_str
+      stderr_str = stderr.read   # read stderr to string
+      status = wait_thr.value    # will block until the command finishes; returns status that responds to .success? etc
+
+      if status.success?
+        File.open("#{dir}/pancritic.pdf") do |file|
+          @stuff = Stuff.create(filename: "pancritic_editor #{DateTime.now}")
+          @stuff.file.attach(io: file, filename: "basic-markdown-editor-#{Date.today.to_s}.pdf")
+        end
+      else
+        @errormessage = stderr_str
+      end
+
+
+    end
+
+
+
+    File.delete("#{dir}/pancritic.md")
+    File.delete("#{dir}/pancritic.pdf")
+
+
+  end
+
+  def editor
+
+    if params[:search_inputs].present?
+      @search_inputs = OpenStruct.new(params[:search_inputs])
+    else
+      @search_inputs = OpenStruct.new()
+    end
+
+    @search_inputs[:text] = @search_inputs[:text] ? @search_inputs[:text] : "<h1>Title</h1>
+<p>Normal</p>
+<p><em>Italic</em></p>
+<p><strong>Bold</strong></p>
+<h2>List</h2>
+<ul>
+<li>a</li>
+<li>b</li>
+<li>c</li>
+<li>d</li>
+</ul>"
+
+    markdown_text = @search_inputs[:text]
+
+    @markdown_text = Kramdown::Document.new(' <h1 id="header">Header</h1> <p>This is <span style="color: red">written in red</span>.</p>').to_kramdown
+
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+
+    @result = markdown.render(markdown_text)
+
+    #@result = Kramdown::Document.new(markdown_text).to_html
+
+  end
+
+
+  def basic_markdown_editor_old
+    if params[:inputs].present?
+      @inputs = OpenStruct.new(params[:inputs])
+    else
+      @inputs = OpenStruct.new()
+    end
+
+    start_markdown ='
+---
+title: Logic and Metaphysics
+shorttitle: What is the connection between language and being?
+author: Sandro Räss
+date: May 9, 2020
+fontfamily: lmodern,changes
+header-includes:
+  \paperheight = 29.70 cm  \paperwidth = 21.0 cm  \hoffset        = 0.46 cm
+  \headheight  =  0.81 cm  \textwidth  = 15.0 cm  \evensidemargin = 0.00 cm
+  \headsep     =  0.81 cm  \textheight = 9.00 in  \oddsidemargin  = 0.00 cm
+---
+# Introduction
+
+If a then b
+
+## Models of Language
+
+*italics* and _italics_
+**bold** and __bold__
+***bold it.*** and ___bold it.___
+
+```
+this is
+a
+code block
+```
+
+
+'
+
+    @inputs[:text] = @inputs[:text] ? @inputs[:text] : start_markdown
+
     @stuff = Stuff.create(filename: "basic-markdown-editor #{DateTime.now}")
 
     dir = Rails.root.join('public', 'pandocin')
@@ -94,7 +260,7 @@ code block
 
   end
 
-  def pancritic_editor
+  def pancritic_editor_old
     if params[:inputs].present?
       @inputs = OpenStruct.new(params[:inputs])
     else
@@ -133,7 +299,7 @@ Cum sociis natoquel {--penatibus et magnis--}{>>FTP - 2013-05-13 08:20:18<<} dis
     #  @result = PandocRuby.convert(@inputs[:text], :from => :markdown, :to => :pdf)
     #end
 
-    file_to_store = Tempfile.new('basic-markdown-editor-#{Date.today.to_s}.pdf')
+    #file_to_store = Tempfile.new('basic-markdown-editor-#{Date.today.to_s}.pdf')
     #file_to_store.write(@result)
     #file_to_store.rewind
     @stuff = Stuff.create(filename: "pancritic_editor #{DateTime.now}")
@@ -156,41 +322,8 @@ Cum sociis natoquel {--penatibus et magnis--}{>>FTP - 2013-05-13 08:20:18<<} dis
     File.delete("#{dir}/pancritic.md")
     File.delete("#{dir}/pancritic.pdf")
 
-
     #file_to_store.close
 
-
-  end
-
-  def editor
-
-    if params[:search_inputs].present?
-      @search_inputs = OpenStruct.new(params[:search_inputs])
-    else
-      @search_inputs = OpenStruct.new()
-    end
-
-    @search_inputs[:text] = @search_inputs[:text] ? @search_inputs[:text] : "<h1>Title</h1>
-<p>Normal</p>
-<p><em>Italic</em></p>
-<p><strong>Bold</strong></p>
-<h2>List</h2>
-<ul>
-<li>a</li>
-<li>b</li>
-<li>c</li>
-<li>d</li>
-</ul>"
-
-    markdown_text = @search_inputs[:text]
-
-    @markdown_text = Kramdown::Document.new(' <h1 id="header">Header</h1> <p>This is <span style="color: red">written in red</span>.</p>').to_kramdown
-
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-
-    @result = markdown.render(markdown_text)
-
-    #@result = Kramdown::Document.new(markdown_text).to_html
 
   end
 end
