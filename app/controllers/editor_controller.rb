ef518abf04848c230fec626c58d6c88af103b1a8
Metaphysiker <s.raess@me.com>
2020-10-17 22:16:26 +0200

    pandoc version

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -134,6 +134,7 @@ Cum sociis natoquel {--penatibus et magnis--}{>>FTP - 2013-05-13 08:20:18<<} dis
     puts panv
     logger.info panv
 
+    puts "Pandoc version"
     pdflatex = %x(pandoc -v)
     logger.info pdflatex
 
