36bac13f753880f96ab5f727638b38a8d8db62e3
Metaphysiker <s.raess@me.com>
2020-10-18 20:11:24 +0200

    logged out pdflatex

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -139,9 +139,9 @@ Cum sociis natoquel {--penatibus et magnis--}{>>FTP - 2013-05-13 08:20:18<<} dis
     pandoc = %x(pandoc -v)
     logger.info pandoc
 
-    puts "PDFlatex version"
-    pdflatex = %x(pdflatex -v)
-    logger.info pdflatex
+    #puts "PDFlatex version"
+    #pdflatex = %x(pdflatex -v)
+    #logger.info pdflatex
 
     puts "Here comes the output! Here comes the output! Here comes the output!"
     puts output
