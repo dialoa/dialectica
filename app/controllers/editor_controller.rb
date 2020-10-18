a603fdad40489147d826f67eb37422a623ba55c7
Metaphysiker <s.raess@me.com>
2020-10-18 23:04:18 +0200

    removed engige

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -127,7 +127,7 @@ Cum sociis natoquel {--penatibus et magnis--}{>>FTP - 2013-05-13 08:20:18<<} dis
 
 
 
-    `pancritic -s -o #{dir}/pancritic.pdf #{dir}/pancritic.md --pdf-engine=/home/sandro/.local/bin/texlive/bin/x86_64-linux/pdflatex`
+    `pancritic -s -o #{dir}/pancritic.pdf #{dir}/pancritic.md`
 
     #%x(pancritic -s -o #{dir}/pancritic.pdf #{dir}/pancritic.md)
 
