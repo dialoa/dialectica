cf80f657db24154a65977b4c434d5c2e86067efb
Metaphysiker <s.raess@me.com>
2020-11-03 17:37:01 +0100

    maybe json database

--- app/jobs/compare_author_bibtex_with_crossref_job.rb
+++ app/jobs/compare_author_bibtex_with_crossref_job.rb
@@ -32,6 +32,79 @@ class CompareAuthorBibtexWithCrossrefJob < ApplicationJob
           serrano = Serrano.works(query: citation_of_result_from_bibtex)
 
           serrano["message"]["items"].first(10).each do |item|
+            bibtex_entry_of_author.children.create(content: item)
+          end
+        rescue
+          @retries ||= 0
+          if @retries < 3
+            @retries += 1
+            puts "ERROR!!! RETRY: #{@retries}"
+            sleep 500
+            retry
+          else
+            #file = file + "ERROR for: #{line}"
+          end
+        end
+          #serrano = Serrano.works(query: result_from_bibtex)
+          #result_from_crossref = + Serrano.content_negotiation(ids: serrano["message"]["items"].first["DOI"], format: "text", style: "apa").force_encoding(Encoding::UTF_8)
+
+        #else
+        #  result_from_crossref = Serrano.content_negotiation(ids: article.doi, style: "apa", format: "text").force_encoding(Encoding::UTF_8)
+        #
+        #  result_from_crossref = change_id_of_bibtex_entry(article.id, result_from_crossref)
+        #
+        #  bibtex_entry_of_author.children.create(content: result_from_crossref.to_s.strip)
+        #end
+
+        #serrano["message"]["items"].first(10).each do |item|
+        #  BibtexEntry.create(content: Serrano.content_negotiation(ids: item["DOI"], format: "text", style: "apa").force_encoding(Encoding::UTF_8))
+        #end
+
+        #serrano = Serrano.works(query: line)
+        #result_from_crossref = Serrano.content_negotiation(ids: article.doi, style: "apa", format: "text").force_encoding(Encoding::UTF_8)
+
+        #@result.push([result_from_bibtex, result_from_crossref])
+        #@array_of_bibtex_originals.push(bibtex_entry_of_author)
+        @array_of_bibtex_originals = @array_of_bibtex_originals + ", #{bibtex_entry_of_author.id}"
+      end
+
+
+      file_to_store.close
+
+      BibtexMailer.bibtex_is_ready_to_compare_email(@array_of_bibtex_originals, email).deliver_now
+  end
+
+  def perform_old(text, email)
+    #@array_of_bibtex_originals = []
+    @array_of_bibtex_originals = ""
+
+    #@result = []
+
+      cp = CiteProc::Processor.new style: 'apa', format: 'text'
+
+      file_to_store = Tempfile.new('comparison')
+      file_to_store.write(text)
+      file_to_store.rewind
+
+      b = BibTeX.open(file_to_store)
+      cp.import BibTeX.open(file_to_store).to_citeproc
+
+      #every article in the author's bibtex file gets scanned
+      b.each_with_index do |article, index|
+        #result_from_bibtex = (cp.render :bibliography, id: article.id).first
+        result_from_bibtex = article
+        bibtex_entry_of_author = BibtexEntry.create(content: result_from_bibtex)
+
+        result_from_crossref = ""
+        citation_of_result_from_bibtex = (cp.render :bibliography, id: article.id).first
+        #byebug
+        #check if doi is available
+        #if article.try(:doi).nil? || article.doi.blank?
+        begin
+          #serrano = Serrano.works(query: result_from_bibtex)
+          serrano = Serrano.works(query: citation_of_result_from_bibtex)
+
+          serrano["message"]["items"].first(10).each do |item|
             begin
               result_from_crossref = Serrano.content_negotiation(ids: item["DOI"], format: "text", style: "bibtex").force_encoding(Encoding::UTF_8)
               #byebug
