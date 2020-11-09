f386647958008753b94f4924de9daca87ee05e59
Metaphysiker <s.raess@me.com>
2020-11-09 20:23:21 +0100

    password changed for production

--- config/environments/productionheroku.rb
+++ config/environments/productionheroku.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:             'dialecticaphilosophie@gmail.com',
-    password:              Rails.application.credentials.gmail_password,
+    password:              ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true }
 
