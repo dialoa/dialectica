d6f4e3f2363e1aa225fd608a9784b3c3ac24caf3
Metaphysiker <s.raess@me.com>
2020-11-09 20:27:39 +0100

    changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,5 +119,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'dialectica-philosophy.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'http://159.65.120.231/'}
 end
