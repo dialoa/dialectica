b8f003faf3c6786e64edd4ebbdbb89196ca9f9bf
Metaphysiker <s.raess@me.com>
2020-12-12 21:21:45 +0100

    edit and destroy

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -42,7 +42,7 @@ class HistoriesController < ApplicationController
   def update
     respond_to do |format|
       if @history.update(history_params)
-        format.html { redirect_to @history, notice: 'History was successfully updated.' }
+        format.html { redirect_to submission_path(@history.submission), notice: 'History was successfully updated.' }
         format.json { render :show, status: :ok, location: @history }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class HistoriesController < ApplicationController
   def destroy
     @history.destroy
     respond_to do |format|
-      format.html { redirect_to histories_url, notice: 'History was successfully destroyed.' }
+      format.html { redirect_to submission_pool_url, notice: 'History was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
