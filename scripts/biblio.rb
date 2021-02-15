559c646dfb25f782a36f622f44238cb589a67980
Metaphysiker <s.raess@me.com>
2021-02-16 00:11:25 +0100

    added bibliography

---
+++ scripts/biblio.rb
@@ -0,0 +1,6 @@
+Ich habe eine kleine Frage: wie kompliziert ist es, ein Skript zu schreiben (das man bspw vom Terminal aus starten kann), das als Input eine Liste von bib-tex keys nimmt:
+
+rowland_c:2008
+dawson_a:1999
+
+etc und ein bib-file ausgibt, das aus denjenigen Einträgen im grossen biblio.bib besteht, die diese keys haben? Ich glaube, dass das Skript, das wir auf switchdrive/BibTeX-scripts haben, so etwas macht, vielleicht lässt sich das einfach vereinfachen? 
