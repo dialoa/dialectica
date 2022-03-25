63ad8b070039ca9a0960b78280ff92fcab001105
Metaphysiker <s.raess@me.com>
2022-03-25 09:28:33 +0100

    precompile

---
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rails assets:clobber
+RAILS_ENV=production rails assets:precompile
+git add .
+git commit -m "precompile"
+git push origin main
+
+ssh sandro@159.65.120.231 << EOF
+  cd dialectica
+  git pull origin master
+  bundle config set --local without 'development test'
+  bundle install
+  RAILS_ENV=production rails db:migrate
+EOF
+
+ssh root@159.65.120.231 << EOF
+  cd /home/sandro/dialectica/
+  sudo service nginx restart
+EOF
