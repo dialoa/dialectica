a1d393b4bce57ecafded9af6a7ab65c781c15453
Metaphysiker <s.raess@me.com>
2022-07-02 11:14:56 +0200

    tests pass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :welcome]
 
   # GET /pages
   # GET /pages.json
