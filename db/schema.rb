7e48a90f16ab2d7f2c104dcffe877778f3b64dec
Metaphysiker <s.raess@me.com>
2022-07-30 20:06:53 +0200

    fixed show of history

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_170909) do
+ActiveRecord::Schema.define(version: 2022_07_30_171818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 2022_07_02_170909) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "alternative_name", default: ""
     t.index ["submission_id"], name: "index_histories_on_submission_id"
     t.index ["user_id"], name: "index_histories_on_user_id"
   end
