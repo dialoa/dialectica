3e7d72e50e7633f111154affb1609972d82cca8d
Metaphysiker <s.raess@me.com>
2022-12-01 00:26:43 +0100

    format and script change

--- deploy-on-digital-ocean-from-master-branch.sh
+++ deploy-on-digital-ocean-from-master-branch.sh
@@ -8,6 +8,7 @@ git push origin master
 
 ssh sandro@159.65.120.231 << EOF
   cd dialectica
+  git stash
   git pull origin master
   bundle config set --local without 'development test'
   bundle install
@@ -18,3 +19,5 @@ ssh root@159.65.120.231 << EOF
   cd /home/sandro/dialectica/
   sudo service nginx restart
 EOF
+
+yarn install --check-files
